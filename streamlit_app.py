26534f6977d78ee224a31f4e18e35394dbfe45c1
withken <withken@users.noreply.github.com>
2023-03-25 12:43:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('🥑🍞 Avocado Torst')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index)) 
 streamlit.dataframe(my_fruit_list)

b7c47f97f27cd1e87e3d93a868947c78a37b69fe
withken <withken@users.noreply.github.com>
2023-03-25 12:29:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Torst')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
